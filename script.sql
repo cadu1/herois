4d3366c0009fb9aa34731d73dcceadf4eed9efcc
Carlos Oliveira <carlos.eduardo02@gmail.com>
2018-08-08 21:19:38 -0300

    Correções no relacionamento do BD

--- script.sql
+++ script.sql
@@ -1,5 +1,5 @@
 -- MySQL Workbench Synchronization
--- Generated: 2018-08-08 21:02
+-- Generated: 2018-08-08 21:17
 -- Model: New Model
 -- Version: 1.0
 -- Project: Name of the project
@@ -19,12 +19,12 @@ CREATE TABLE IF NOT EXISTS `herois`.`personagem` (
   `per_dano` INT(11) NOT NULL,
   `per_vel_ataque` DECIMAL(19,2) NOT NULL,
   `per_vel_mov` INT(11) NOT NULL,
-  `id_especialidade` INT(11) NOT NULL,
+  `id_tipo` INT(11) NOT NULL,
   PRIMARY KEY (`id_personagem`),
-  INDEX `fk_especialidade_idx` (`id_especialidade` ASC),
-  CONSTRAINT `fk_especialidade`
-    FOREIGN KEY (`id_especialidade`)
-    REFERENCES `herois`.`especialidade` (`id_especialidade`)
+  INDEX `fk_personagem_tipo1_idx` (`id_tipo` ASC),
+  CONSTRAINT `fk_personagem_tipo1`
+    FOREIGN KEY (`id_tipo`)
+    REFERENCES `herois`.`tipo` (`id_tipo`)
     ON DELETE RESTRICT
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -62,25 +62,6 @@ VALUES (1,"Mago"),
 (5,"Cavaleiro"),
 (6,"Espadachim");
 
-CREATE TABLE IF NOT EXISTS `herois`.`tipo_personagem` (
-  `id_tipo` INT(11) NOT NULL,
-  `id_personagem` INT(11) NOT NULL,
-  PRIMARY KEY (`id_tipo`, `id_personagem`),
-  INDEX `fk_tipo__personagem_personagem1_idx` (`id_personagem` ASC),
-  INDEX `fk_tipo_personagem_tipo1_idx` (`id_tipo` ASC),
-  CONSTRAINT `fk_tipo_personagem_tipo1`
-    FOREIGN KEY (`id_tipo`)
-    REFERENCES `herois`.`tipo` (`id_tipo`)
-    ON DELETE RESTRICT
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tipo_personagem_personagem1`
-    FOREIGN KEY (`id_personagem`)
-    REFERENCES `herois`.`personagem` (`id_personagem`)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
 CREATE TABLE IF NOT EXISTS `herois`.`avatar` (
   `id_avatar` INT(11) NOT NULL AUTO_INCREMENT,
   `ava_img` BLOB NOT NULL,
@@ -88,6 +69,25 @@ CREATE TABLE IF NOT EXISTS `herois`.`avatar` (
   PRIMARY KEY (`id_avatar`),
   INDEX `fk_personagem1_idx` (`id_personagem` ASC),
   CONSTRAINT `fk_avatar_personagem1`
+    FOREIGN KEY (`id_personagem`)
+    REFERENCES `herois`.`personagem` (`id_personagem`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `herois`.`especialidade_personagem` (
+  `id_especialidade` INT(11) NOT NULL,
+  `id_personagem` INT(11) NOT NULL,
+  PRIMARY KEY (`id_especialidade`, `id_personagem`),
+  INDEX `fk_especialidade_has_personagem_personagem1_idx` (`id_personagem` ASC),
+  INDEX `fk_especialidade_has_personagem_especialidade1_idx` (`id_especialidade` ASC),
+  CONSTRAINT `fk_especialidade_has_personagem_especialidade1`
+    FOREIGN KEY (`id_especialidade`)
+    REFERENCES `herois`.`especialidade` (`id_especialidade`)
+    ON DELETE RESTRICT
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_especialidade_has_personagem_personagem1`
     FOREIGN KEY (`id_personagem`)
     REFERENCES `herois`.`personagem` (`id_personagem`)
     ON DELETE CASCADE
